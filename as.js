28b7e5ce2ff76714585832174d7336c0634a2842
sammihung <156680161+sammihung@users.noreply.github.com>
2024-01-17 22:18:53 +0800

    Add files via upload

---
+++ as.js
@@ -0,0 +1,9 @@
+
+    function displayText() {
+      // Get the user input value
+      var userInput = document.getElementById('userInput').value;
+
+      // Display the user input
+      document.getElementById('displayText').textContent = userInput;
+    }
+     
---
+++ script.js
@@ -0,0 +1,12 @@
+var number = 10;
+var string= 'Hello There';
+var isRad=true;
+if(number==10){
+                    console.log('Yeah buddy');
+}else
+{console.log('Nope!');}
+document.getElementById('box').innerHTML=number+5;
+
+document.getElementById('box').addEventListener('click',function(){
+                    alert('i got clicked');
+});
